e6e7589de6cbe8d8992b851ff42f60392f974b8e
UniStar903 <mygithub.903@gmail.com>
2025-02-15 22:58:47 +0530

    restructured the hierarchy of the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './amazon.png';
 import React, {Component} from 'react';
 import classes from './App.module.css';
-import ProductData from './ProductData';
-import ProductDetails from './ProductDetails';
-import ProductReview from './ProductReview';
-import TopNav from './TopNav';
+import ProductData from './Utils/ProductData';
+import ProductDetails from './ProductDetails/ProductDetails';
+import ProductReview from './ProductReview/ProductReview';
+import TopNav from './Topbar/TopNav';
 
 class App extends Component {
   state = {
-    productData: ProductData
+    productData: ProductData,
+    currentPreviewImage: 'https://imgur.com/iOeUBV7.png',
+    showHeartBeatSection: false 
   }
 
   render(){
@@ -16,7 +18,7 @@ class App extends Component {
     <div className="App">
         <TopNav/>
       <div className={classes.MainContainer}>
-          <ProductReview/>
+          <ProductReview currentPreviewImage={this.state.currentPreviewImage} showHeartBeatSection={this.state.showHeartBeatSection}/>
           <ProductDetails data = {this.state.productData}/>
       </div>
     </div>
